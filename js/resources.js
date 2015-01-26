f9c2c773554f9b40de030ff0db40532e8b3774ef
Michael Cerezo <cerezo.michael@gmail.com>
2015-01-26 11:55:24 -0800

    video 11 coded

--- js/resources.js
+++ js/resources.js
@@ -9,6 +9,7 @@ game.resources = [
 	{name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
 	{name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
 	{name: "player", type:"image", src: "data/img/orcSpear.png"},
+	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
 
 
 	/* Atlases 

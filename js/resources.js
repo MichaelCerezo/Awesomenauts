ec443bbe7d3481f0ecf0cc26fd99a141c31a9eb9
Michael Cerezo <cerezo.michael@gmail.com>
2015-02-09 21:21:48 -0800

    video 26 coded

--- js/resources.js
+++ js/resources.js
@@ -11,6 +11,7 @@ game.resources = [
 	{name: "player", type:"image", src: "data/img/orcSpear.png"},
 	{name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
 	{name: "creep1", type:"image", src: "data/img/brainmonster.png"},
+	{name: "title-screen", type:"image", src: "data/img/title2.png"},
 
 	/* Atlases 
 	 * @example

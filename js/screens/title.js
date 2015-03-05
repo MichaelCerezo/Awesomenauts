e5629cdc6a471676e226630d92e651f8c317dcb1
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-03-05 11:23:29 -0800

    video 40 coded

--- js/screens/title.js
+++ js/screens/title.js
@@ -31,7 +31,26 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 
-		
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function(){
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				this.font = new me.Font("Arial", 46, "white");
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+			},
+
+			draw: function(renderer){
+				this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt){
+				return true;
+			},
+
+			newGame: function(){
+				me.input.releasePointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY);
+			}
+		})));
 		// me.audio.playTrack("Awesomenauts");
 	},
 	

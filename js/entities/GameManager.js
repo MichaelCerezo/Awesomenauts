7e1a7550f97fa4452fd77628b29aa589acf27de0
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-03-05 10:57:35 -0800

    video 38 coded

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -59,14 +59,22 @@ game.ExperienceManager = Object.extend({
 
 	update: function(){
 		if(game.data.win === true && !this.gameOver){
-			game.data.exp += 10;
-			this.gameOver = true;
+			this.gameOver(true);
 		}else if(game.data.win === false && !this.gameOver){
-			game.data.exp += 1;
-			this.gameOver = true;
+			this.gameOver(true);
 		}
-		console.log(game.data.exp);
 
 		return true;
+	},
+
+	gameOver: function(win){
+		if (win) {
+			game.data.exp += 10;
+		}else{
+			game.data.exp += 1;
+		}
+		
+		this.gameOver = true;
+		me.save.exp = game.data.exp;
 	}
 });
--- js/game.js
+++ js/game.js
@@ -44,6 +44,8 @@ var game = {
 		});
 	}
 
+	me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
+
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
 	

d9e2d0c49824b56d79dcd52aec5a13652120a47a
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-03-26 19:45:22 -0700

    video 51 coded

---
+++ gamemanagers/GameTimeManager.js
@@ -0,0 +1,1 @@
+GameTimeManager.js
--- index.php
+++ index.php
@@ -31,7 +31,10 @@
 		<script type="text/javascript" src="js/entities/entities.js"></script>
 		<script type="text/javascript" src="js/entities/PlayerBaseEntity.js"></script>
 		<script type="text/javascript" src="js/entities/IntermediateWork.js"></script>
-		<script type="text/javascript" src="js/entities/GameManager.js"></script>
+		<script type="text/javascript" src="js/gamemanagers/GameManager.js"></script>
+		<script type="text/javascript" src="js/gamemanagers/GameTimeManager.js"></script>
+		<script type="text/javascript" src="js/gamemanagers/SpendGold.js"></script>
+		<script type="text/javascript" src="js/gamemanagers/HeroDeathManager.js"></script>
 		<script type="text/javascript" src="js/entities/EnemyCreep.js"></script>
 		<script type="text/javascript" src="js/entities/EnemyBaseEntity.js"></script>
 		<script type="text/javascript" src="js/entities/HUD.js"></script>

4475764673584804f4e04d9091826ed9eeb90e80
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-03-27 09:42:01 -0700

    fixed video 52

--- gamemanagers/GameTimeManager.js
+++ gamemanagers/GameTimeManager.js
@@ -1,1 +1,37 @@
-GameTimeManager.js
+// Handles all the timers
+game.GameTimerManager = Object.extend({
+	init: function(x, y, settings){
+		this.now = new Date().getTime();
+		// Keeps track of the last time a creep was made
+		this.lastCreep = new Date().getTime();
+		this.paused = false;
+		// Makes sure the game is always updating
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		// Keeps track of timer
+		this.now = new Date().getTime();
+		this.goldTimerCheck();
+		this.creepTimerCheck();
+
+		return true;
+	},
+
+	goldTimerCheck: function(){
+		if(Math.round(this.now/1000) %20 ===0 && (this.now - this.lastCreep >= 1000)){
+			game.data.gold += 1;
+			console.log("Current gold: " + game.data.gold);
+		}
+	},
+
+	creepTimerCheck: function(){
+		if(Math.round(this.now/1000) %10 ===0 && (this.now - this.lastCreep >= 1000)){
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 2350, 500, {});
+			me.game.world.addChild(creepe, 5);
+			var creepe2 = me.pool.pull("PlayerCreep", 200, 450, {});
+			me.game.world.addChild(creepe2, 5);
+		}
+	}
+});

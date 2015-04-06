702c0a7aba7e592eb9c72d0351c746fbb027e46e
Michael Cerezo <cerezo.michael.mc@gmail.com>
2015-04-05 17:55:53 -0700

    video 58 coded

--- js/gamemanagers/GameManager.js
+++ js/gamemanagers/GameManager.js
@@ -7,8 +7,10 @@ game.ExperienceManager = Object.extend({
 	update: function(){
 		if(game.data.win === true && !this.gameover){
 			this.gameOver(true);
+			alert("YOU WIN!");
 		}else if(game.data.win === false && !this.gameover){
 			this.gameOver(true);
+			alert("YOU LOSE!");
 		}
 
 		return true;
@@ -23,9 +25,31 @@ game.ExperienceManager = Object.extend({
 		
 		this.gameover = true;
 		me.save.exp = game.data.exp;
-		// For testing purposes only
-		me.save.exp2 = 4;
-	}
+		
+
+			$.ajax({
+				type: "POST",
+				url: "php/controller/save-user.php",
+				data: {
+					exp:game.data.exp,
+					exp1:game.data.exp1,
+					exp2:game.data.exp2,
+					exp3:game.data.exp3,
+					exp4:game.data.exp4,
+				},
+				dataType: "text"
+			})
+			.success(function(response){
+				if (response==="true"){
+					me.state.change(me.state.MENU);
+				}else{
+					alert(response);
+				}
+			})
+			.fail(function(response){
+				alert("Fail");
+			});
+		}
 });
 
 
